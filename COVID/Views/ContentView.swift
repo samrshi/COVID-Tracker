694bac3b0a1e9372537894be0f2434d16a4c193f
samrshi <samrshi@outlook.com>
2020-08-19 00:12:43 -0400

    updated contentview

--- COVID/Views/ContentView.swift
+++ COVID/Views/ContentView.swift
@@ -1,19 +1,17 @@
 //
 //  ContentView.swift
-//  COVID
+//  Playground
 //
-//  Created by hawkeyeshi on 7/8/20.
+//  Created by hawkeyeshi on 8/5/20.
 //  Copyright © 2020 samrshi. All rights reserved.
 //
 
 import SwiftUI
-import SwiftUICharts
 
 struct ContentView: View {
-    @ObservedObject var historicCountry = HistoricCountryViewModel()
-    @ObservedObject var historicStates = HistoricalStatesViewModel()
-    @ObservedObject var userSettings = UserSettings()
+    @Environment(\.colorScheme) var colorScheme
     
+<<<<<<< Updated upstream
     @State private var showAlert = false
     @State private var alertTitle = ""
     @State private var alertMessage = ""
@@ -68,11 +66,83 @@ struct ContentView: View {
             }
         }
 =======
+    @ObservedObject var data = CovidData()
+    @ObservedObject var news = NewsData()
+    
+    var body: some View {
+        NavigationView {
+            ScrollView(.vertical) {
+                ZStack {
+                    Color.backgroundDark
+                        .edgesIgnoringSafeArea(.all)
+                    
+                    VStack(alignment: .leading) {
+                        VStack {
+                            SectionTitle(
+                                title: "Global Statistics",
+                                image: Image("globe"),
+                                backgroundColor: Color.backgroundLight
+                            )
+                            
+                            GlobalStatsView().environmentObject(data)
+                        }
+                        .padding(.vertical)
+                        
+                        VStack(alignment: .leading) {
+                            Text("Latest News")
+                                .foregroundColor(.white)
+                                .fontWeight(.bold)
+                                .scaledFont(size: 20)
+                            
+                            ForEach(news.newsData.articles, id: \.id) { article in
+                                HStack {
+                                    URLImage(url: article.urlToImage ?? "google")
+                                        .cornerRadius(5)
+                                    
+                                    VStack(alignment: .leading) {
+                                        Text(article.title ?? "-")
+                                            .font(.caption)
+                                            .fontWeight(.semibold)
+                                        
+                                        Text(article.source.name ?? article.author ?? "-")
+                                            .scaledFont(size: 10)
+                                    }
+                                    .foregroundColor(.white)
+                                    
+                                    Spacer()
+                                }
+                            }
+                        }
+                        .padding()
+                        .background(Color.backgroundLight)
+                        .cornerRadius(20)
+                    }
+                    .padding(.horizontal)
+                }
+            }
+            .navigationBarTitle("Covidity")
+        }
+    }
+    
+    init() {
+        let coloredNavAppearance = UINavigationBarAppearance()
+        let backgroundDark = UIColor(named: "backgroundDark")
+        
+        
+        coloredNavAppearance.backgroundColor = backgroundDark
+        coloredNavAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+        coloredNavAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        coloredNavAppearance.shadowColor = .clear
+        
+        UINavigationBar.appearance().standardAppearance = coloredNavAppearance
+        UINavigationBar.appearance().scrollEdgeAppearance = coloredNavAppearance
+        UIScrollView.appearance().backgroundColor = backgroundDark
+>>>>>>> Stashed changes
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.colorScheme, .dark)
+        ContentView()
     }
 }
